d23cb52c46767a54d629cc453c4249a68b375855
Ranjeet Kumar <ranjeet93ks@gmail.com>
2023-11-30 23:41:38 +0530

    Revert "commit from eclipse"
    This reverts commit 8847f491583e41b85ad21523422139a73b1b0200.

--- src/main/java/com/ranjs/spring/batch/service/JobService.java
+++ src/main/java/com/ranjs/spring/batch/service/JobService.java
@@ -21,9 +21,8 @@ public class JobService {
 	private Job job;
 
 	public void importCsvToDBJob() {
-		//select * from  batch_job_execution_params;
 		JobParameters jobParameters = new JobParametersBuilder().addLong("startAt", System.currentTimeMillis())
-				.addString("Country", "India").toJobParameters();
+				.toJobParameters();
 		try {
 			jobLauncher.run(job, jobParameters);
 		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
